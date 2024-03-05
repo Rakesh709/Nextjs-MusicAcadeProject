46a9eb3904536977b0e3fa545b1d635732f71684
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-03-06 00:22:30 +0530

    Navbar component added

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,40 @@
+"use client";
+//because we cant use react hook in next js to render something on render side use client side
+
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/utils/cn";
+import Link from "next/link";
+
+function Navbar({ className }: { className?: string }) {
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+        <Link href={"/"}>
+          <MenuItem setActive={setActive} active={active} item="Home">
+            {/* //what do you want to give inside this */}
+          </MenuItem>
+        </Link>
+        <MenuItem setActive={setActive} active={active} item="Our Courses">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/courses">All Courses</HoveredLink>
+            <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
+            <HoveredLink href="/courses">Advance Composition</HoveredLink>
+            <HoveredLink href="/courses">Song Writing</HoveredLink>
+            <HoveredLink href="/courses">Music Production</HoveredLink>
+          </div>
+        </MenuItem>  
+        <Link href={"/contact"}>
+            <MenuItem setActive={setActive} active={active} item="Contact Us">
+              {/* //create contact page */}
+            </MenuItem>
+        </Link>   
+      </Menu>
+    </div>
+  );
+}
+
+export default Navbar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,8 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
+  // to enable dark mode make changes here
   theme: {
     extend: {
       backgroundImage: {
