e83a7495b3eb8c539576b9664c30091044fdfa24
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-01-04 00:46:58 +0530

    removed top name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.03]"> 
         {/* for grid plugin need to import */}
-       <h1 className="text-3xl text-center">Rakesh Music Academy</h1>
+       {/* <h1 className="text-3xl text-center">Rakesh Music Academy</h1> */}
        <HeroSection/>
        <FeaturedCourses/>
        <ThreeDCardDemo/>
