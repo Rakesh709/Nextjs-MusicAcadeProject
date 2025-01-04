7e506658a56811cf8f8390e5f58277c37f2bc11c
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-01-04 19:38:12 +0530

    instructor section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import FeaturedCourses from "@/components/FeaturedCourses";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import TestimonialCards from "@/components/TestimonialCards";
 import UpcommingWebinar from "@/components/UpcommingWebinar";
+import Instructors from "@/components/Instructors";
 
 
 export default function Home() {
@@ -16,6 +17,7 @@ export default function Home() {
        <WhyChooseUs/>
        <TestimonialCards/>
        <UpcommingWebinar/>
+       <Instructors/>
        <ThreeDCardDemo/>
 
        
